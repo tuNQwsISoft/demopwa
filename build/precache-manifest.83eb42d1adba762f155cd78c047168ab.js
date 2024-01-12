54c2eebc70331158b70588ed885b1a80ffd40f9b
VanSy27 <sybui@i-soft.com.vn>
2024-01-12 17:06:40 +0700

    add build folder

---
+++ build/precache-manifest.83eb42d1adba762f155cd78c047168ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22dbc0ccb352bf95447a99313148f2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "403e6cbd39c6d944261f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f327096c66c64a14d9d9",
+    "url": "/static/js/2.9f463b17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f463b17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403e6cbd39c6d944261f",
+    "url": "/static/js/main.ff163a5d.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
